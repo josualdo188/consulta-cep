d9f386728c1b5127f17941d1f9016ee56aace609
josualdo188 <josualdosds@gmail.com>
2023-03-07 21:49:49 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function consultaEndereco() {
+   let cep = document.querySelector('#cep').value
+    if (cep.length !==8) {
+        alert('CEP Inválidao!')
+        return
+    }
+   let url = `https://viacep.com.br/ws/${cep}/json/`
+   fetch(url).then(function(response){
+    response.json().then(mostrarEndereco)     
+   })
+   
+}
+function mostrarEndereco(dados) {
+    let resultado = document.querySelector('#resultado')
+    if (dados.erro) {
+        resultado.innerHTML = ("Não foi possivel localizar endereço")
+    } else {
+
+    resultado.innerHTML =  `<p>Endereço: ${dados.logradouro}</p>
+                            <p>Complemento: ${dados.complemento}</p>
+                            <p>Bairro: ${dados.bairro}</p>
+                            <p>Cidade: ${dados.localidade} - ${dados.uf }</p>`
+    }
+}
